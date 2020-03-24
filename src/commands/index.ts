095f3c9bd22b2a63da31d5586fd9c7f7731b4b43
Peter Vitez <peter.vitez@solarwinds.com>
2020-03-24 20:46:07 +0100

    ktojekokot command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,11 +2,13 @@ import Paprik from './paprik';
 import Command from '../command';
 import IQ from './iq';
 import Play from './play';
+import KtoJeKokot from './ktoJeKokot';
 
 const CommandsList: Record<string, typeof Command> = {
   paprik: Paprik,
   iq: IQ,
   play: Play,
+  ktojekokot: KtoJeKokot,
 };
 
 export default CommandsList;
---
+++ src/commands/ktoJeKokot.ts
@@ -0,0 +1,13 @@
+import Command from '../command';
+import { getRandomInt } from '../utils/random';
+
+export default class KtoJeKokot extends Command {
+  async execute() {
+    var userArr = this.client.users.cache;
+    let num: number = getRandomInt(0, userArr.size);
+    console.log(user);
+    var user = userArr.random();
+
+    this.message.channel.send(`<@${user.id}> je kokot.`);
+  }
+}
